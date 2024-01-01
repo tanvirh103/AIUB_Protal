af66a70053f813bc3ca26c07bfa2d76a52991dc8
tanvir103 <tanvirh103@gmail.com>
2024-01-01 23:28:03 +0600

    .

--- Controller/add-student-controller.php
+++ Controller/add-student-controller.php
@@ -1,7 +1,14 @@
 <?php
+require_once('../Model/user-info-model.php');
 if(isset($_POST['submit'])){
     $name=$_POST['username'];
-    echo "$name";
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $dob=$_POST['dob'];
+    $result=addnewstudent($name,$email,$password,$dob);
+    if($result!=false){
+        echo "New Student added";
+    }
 }
 
 ?>
--- Model/user-info-model.php
+++ Model/user-info-model.php
@@ -22,4 +22,10 @@ require_once('database.php');
             return $row;
         }
     }
+    function addnewstudent($name,$email,$password,$dob){
+        $conn=dbConnection();
+        $sql="INSERT INTO userinfo VALUES('','$name','Uploads/default.jpg','$email','$password','Sutdent','$dob','Active','')";
+        $result=mysqli_query($conn,$sql);
+        return true;
+    }
 ?>
