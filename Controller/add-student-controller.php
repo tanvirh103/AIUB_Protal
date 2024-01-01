95b653f90dff5ae7dc3998b7dd4521a21f004164
tanvir103 <tanvirh103@gmail.com>
2024-01-01 23:00:22 +0600

    .

---
+++ Controller/add-student-controller.php
@@ -0,0 +1,7 @@
+<?php
+if(isset($_POST['submit'])){
+    $name=$_POST['username'];
+    echo "$name";
+}
+
+?>
--- View/addstudent.php
+++ View/addstudent.php
@@ -9,7 +9,22 @@
     <title>Add New student</title>
 </head>
 <body>
-    <center><font family="times new roman" size="5">Add New Student</font><br><hr width="30%" bordercolor="#004ca3"></center>
-    
+    <center><font family="times new roman" size="5">Add New Student</font><br><hr color="#004ca3" width="30%" bordercolor="#004ca3"><br></center>
+    <center>
+        <form action="../Controller/add-student-controller.php" method="post">
+        <table border="1" width="30%" align="center" bordercolor="#004ca3" cellpadding="15" cellspacing="0">
+            <tr><td><center><font family="times new roman">
+            Username :&nbsp;<input type="text" name="username" size="30"><br><br>
+            Email :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="email" size="30"><br><br>
+            Password :&nbsp;<input type="password" name="password" size="30"><br><br>
+            Date Of Birth :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;
+            <input type="date" name="dob" align="left"><br><br><br>
+            <button name="submit">Create New account</button><br><br>
+            </font></center></td></tr>
+        </table>
+        </form>
+    </center>
 </body>
 </html>
