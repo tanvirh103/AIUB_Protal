f2c783329c6ad7375c42673b1d496fee04baee62
tanvir103 <tanvirh103@gmail.com>
2024-01-03 05:14:33 +0600

    .

--- View/viewprofileinfo.php
+++ View/viewprofileinfo.php
@@ -19,18 +19,12 @@ $row=getinfo($id);
     <center><table width="auto" cellspacing="0" cellpadding="25" bordercolor="#004ca3" border="1">
         <tr><td><center><font size="4">
             <img src="../<?php echo $row['profilePicture']  ?>" alt="Profile Picture" width="200" height="200"><br><br>
-            Fullname :&nbsp;&nbsp;<input type="text" value="<?php echo$row['userName'] ?>" size="30" align="center" readonly> <br><br>
-            Father's Name :&nbsp;&nbsp;<input type="text" value="<?php echo$row['fathersName'] ?>" size="30" align="center" readonly><br><br>
-            Mother's Name :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            &nbsp;&nbsp;&nbsp;<?php echo$row['mothersName'] ?><br><br>
-            Blood Group :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <?php echo$row['bloodGroup'] ?><br><br>
-            Email :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo$row['email'] ?><br><br>
-            Date Of Birth :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo$row['dob'] ?><br><br>
+            Fullname :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" value="<?php echo$row['userName'] ?>" size="25" align="center" readonly> <br><br>
+            Father's Name :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" value="<?php echo$row['fathersName'] ?>" size="25" align="center" readonly><br><br>
+            Mother's Name :&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" value="<?php echo$row['mothersName'] ?>" size="25" align="center" readonly><br><br>
+            Blood Group :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" value="<?php echo$row['bloodGroup'] ?>" size="25" align="center" readonly><br><br>
+            Email :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" value="<?php echo$row['email'] ?>" size="25" align="center" readonly><br><br>
+            Date Of Birth :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" value="<?php echo$row['dob'] ?>" size="25" align="center" readonly><br><br>
         </center></font></td></tr>
     </table></center>
 </body>
