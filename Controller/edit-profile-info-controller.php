bcc5b5ccc07448a40867a20eb84da228da16ba42
tanvir103 <tanvirh103@gmail.com>
2024-01-07 02:05:35 +0600

    .

---
+++ Controller/edit-profile-info-controller.php
@@ -0,0 +1,12 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $name=$_POST['username'];
+    $fathername=$_POST['fathername'];
+    $mothername=$_POST['mothername'];
+    $bloodgrp=$_POST['bg'];
+    $email=$_POST['email'];
+    $dob=$_POST['dob'];
+
+}
+?>
--- View/editprofileinfo.php
+++ View/editprofileinfo.php
@@ -13,5 +13,16 @@ if(!isset($_COOKIE['flag'])){
 </head>
 <body>
     <center><font family="times new roman" size="6">Edit Profile Info</font><hr width="15%" color="#004ca3"></center>
+    <center><br><form action="../Controller/edit-profile-info-controller.php" method="post"><table width="auto" cellspacing="0" cellpadding="15" bordercolor="#004ca3" border="1">
+    <tr><td><center><font size="4">
+            Fullname :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="username" size="25" align="center"> <br><br>
+            Father's Name :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="fathername" size="25" align="center"><br><br>
+            Mother's Name :&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="mothername" size="25" align="center" ><br><br>
+            Blood Group :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="bg" size="25" align="center" ><br><br>
+            Email :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="email" size="25" align="center"><br><br>
+            Date Of Birth :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="dob" size="25" align="center"><br><br>
+            <input type="submit" name="submit" value="Update Profile Info"><br><br>
+        </center></font></td></tr>
+    </table></form></center>
 </body>
 </html>
