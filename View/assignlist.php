b5e5841c0cac9976d75810899f55d74867e12dcc
Tanvir Hasan Tamal <tanvirh103@gmail.com>
2024-01-15 05:49:24 +0600

    .

--- View/assignlist.php
+++ View/assignlist.php
@@ -19,7 +19,7 @@ $result=getsectionbyfaculty($id);
     <center><font family="times new roman" size="6">Assign Section List</font><br><hr width="20%" color="#004ca3">
     <br><br>
 </center>
-    <center><br><table width="auto" cellspacing="0" cellpadding="15" bordercolor="#004ca3" border="1"><font family="times new roman" size="4">
+    <center><table width="auto" cellspacing="0" cellpadding="15" bordercolor="#004ca3" border="1"><font family="times new roman" size="4">
         <?php if(mysqli_num_rows($result)>0){
          echo"<tr>
             <td>Course ID</td>
