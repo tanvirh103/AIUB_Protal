f1d8949fc4a9eef9c089b95effe4fd6a64cbb352
tanvir103 <tanvirh103@gmail.com>
2024-01-20 07:16:32 +0600

    .

--- Controller/enroll-course-controller.php
+++ Controller/enroll-course-controller.php
@@ -12,7 +12,8 @@ $time=$_POST['time'];
 $teacherid=$_POST['teacherid'];
 $teachername=$_POST['teachername'];
 $result=enrollcourse($userid,$courseid,$coursename,$sectionid,$sectionname,$room,$time,$teacherid,$teachername);
-if($result!=false){
+$result1=addresult($courseid,$coursename,$sectionid,$sectionname,$teacherid,$teachername,$userid);
+if($result!=false && $result1!=false){
     message("Course Registration Successful");
 }else{
     message("Course Registratiion Failed");
--- Model/result-info-model.php
+++ Model/result-info-model.php
@@ -1,2 +1,10 @@
 <?php
+require_once('../Model/database.php');
+function addresult($courseid,$coursename,$sectionid,$sectionname,$teacherid,$teachername,$userid){
+    $conn=dbConnection();
+    $sql="INSERT INTO resultinfo VALUES('$courseid','$coursename','$sectionid','$sectionname','$teacherid','$teachername','$userid','','')";
+    $result=mysqli_query($conn,$sql);
+    return true;
+}
+
 ?>
